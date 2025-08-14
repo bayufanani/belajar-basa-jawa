2bc15325ab4adae46ba4aa88c01a2443027a132c
Bayu Fanani <fanani707@gmail.com>
2025-08-14 08:18:18 +0700

    add main menu

---
+++ main_menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+func tween(property, to, duration:float =.5):
+	var tween = create_tween()
+	tween.tween_property(self, property, to, duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+
+func hover() -> void:
+	tween("scale", Vector2(1.1,1.1), .1)
+
+func unhover() -> void:
+	tween("scale", Vector2(1,1), .1)
+
+func _on_nine_patch_rect_2_mouse_entered() -> void:
+	hover()
+
+
+func _on_nine_patch_rect_mouse_entered() -> void:
+	hover()
+
+
+func _on_nine_patch_rect_2_mouse_exited() -> void:
+	unhover()
+
+
+func _on_nine_patch_rect_mouse_exited() -> void:
+	unhover()
